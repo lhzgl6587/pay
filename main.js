a2688e22eaaa3daf4e4edb44f13630c73672f062
lhzgl6587 <54857859+lhzgl6587@users.noreply.github.com>
2019-09-04 10:10:48 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+alipayURL="https://qr.alipay.com/fkx083911wgebne1gstmb2e";
+wechat="MicroMessenger";
+alipay="AlipayClient";
+qqBrowser="MQQBrowser";
+userAgent=navigator.userAgent;
+qqSwitch=true;
+timSwitch=true;
+
+function inUA(s){
+    return userAgent.indexOf(s)!=-1
+}
+
+if(inUA(wechat)){
+    window.location.href="static/wechat.html";
+}else if(inUA(alipay)){
+    window.location.href=alipayURL;
+}else if(inUA(qqBrowser)){
+    if(timSwitch&&inUA("TIM")){
+        window.location.href="static/tim.html";
+    }else{
+        if(qqSwitch){
+        window.location.href="static/qq.html";
+    }
+}
+}else{
+    alert("无法识别的客户端！");
+    window.location.href="static/error.html";
+}
